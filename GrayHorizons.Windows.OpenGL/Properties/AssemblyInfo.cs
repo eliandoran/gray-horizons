027155a5c97856de0c6f8843626e42cba1366a25
Elian Doran <doran.adoris.elian@gmail.com>
2020-01-04 12:50:21 +0200

    Change assembly info for OpenGL project

--- GrayHorizons.Windows.OpenGL/Properties/AssemblyInfo.cs
+++ GrayHorizons.Windows.OpenGL/Properties/AssemblyInfo.cs
@@ -1,4 +1,6 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Runtime.InteropServices;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 
 #if __ANDROID__
@@ -8,12 +10,12 @@ using Android.App;
 // Information about this assembly is defined by the following attributes.
 // Change them to the values specific to your project.
 
-[assembly: AssemblyTitle("GrayHorizons.Windows.OpenGL")]
+[assembly: AssemblyTitle("Gray Horizons (OpenGL)")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("elinn_000")]
+[assembly: AssemblyProduct("Gray Horizons")]
+[assembly: AssemblyCopyright("© 2015, 2020 by Elian Doran")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -22,6 +24,8 @@ using Android.App;
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
 [assembly: AssemblyVersion("1.0.0")]
+[assembly: ComVisible(false)]
+[assembly: NeutralResourcesLanguage("ro-RO")]
 
 // The following attributes are used to specify the signing key for the assembly,
 // if desired. See the Mono documentation for more information about signing.
